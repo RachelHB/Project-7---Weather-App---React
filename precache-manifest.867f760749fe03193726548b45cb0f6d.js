624de6fa97fbb1c51a11fae3e78076ba8ca0fdad
RachelHB <105057555+RachelHB@users.noreply.github.com>
2022-06-23 11:10:40 +0100

    Updates

---
+++ precache-manifest.867f760749fe03193726548b45cb0f6d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9def8c44549707375add7c38827f2b",
+    "url": "/Project-7---Weather-App---React/index.html"
+  },
+  {
+    "revision": "de1fb883f3222acf140c",
+    "url": "/Project-7---Weather-App---React/static/css/main.88cbb4f1.chunk.css"
+  },
+  {
+    "revision": "f85c171887fd8017aefa",
+    "url": "/Project-7---Weather-App---React/static/js/2.f9a9f70d.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/Project-7---Weather-App---React/static/js/2.f9a9f70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1fb883f3222acf140c",
+    "url": "/Project-7---Weather-App---React/static/js/main.0c2a6328.chunk.js"
+  },
+  {
+    "revision": "f815e75f1d957565cee0",
+    "url": "/Project-7---Weather-App---React/static/js/runtime-main.06cd2eb5.js"
+  },
+  {
+    "revision": "76473821097f938b8651e2a9adb9920d",
+    "url": "/Project-7---Weather-App---React/static/media/background.76473821.png"
+  },
+  {
+    "revision": "2cdff5d733dfe165def30df45164de76",
+    "url": "/Project-7---Weather-App---React/static/media/temp icon white.2cdff5d7.png"
+  },
+  {
+    "revision": "ca7ab37d6ecc6e906842acf87b2d1bcd",
+    "url": "/Project-7---Weather-App---React/static/media/trans hums white.ca7ab37d.png"
+  },
+  {
+    "revision": "4f716f01483345b3a3e498ccc916347f",
+    "url": "/Project-7---Weather-App---React/static/media/weather image 1.4f716f01.jpg"
+  }
+]);
